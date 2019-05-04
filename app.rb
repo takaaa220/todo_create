acfb7b8f6982edde292ff0902b09111eda5d4b1f
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-05-04 15:47:16 +0900

    add

--- app.rb
+++ app.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
+require 'net/http'
+require 'url'
 
 get "/" do
   "hello"
 end
 
 post "/payload" do
-  p request.body.read
+  body = JSON.parse(request.body.read)
+  diff_url = body["compare"] + ".diff"
+  uri = URI.parse(diff_url)
+  response_body = NET::HTTP.get_response(uri).body
+
+  added_diff = response_body.split("\n").select do |line|
+    line[0] == "+" && line[1] != "+" && line[2] != "+"
+  end
+
+  included_todo = added_diff.select { |diff| diff.include?("TODO: ") }
+
+  included_todo.each do |todo|
+    p todo
+  end
 end
