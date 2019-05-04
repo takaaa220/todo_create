937febbb003ca6da2e7e3c02160ee628d3a5710f
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-05-04 16:13:28 +0900

    add

--- app.rb
+++ app.rb
@@ -6,15 +6,10 @@ require 'uri'
 require 'pry'
 
 get "/" do
-  "hello"
-end
-
-post "/payload" do
-  body = JSON.parse(params[:payload])
-  binding.pry
+  body = { "compare" => "https://github.com/takaaa220/todo_create/compare/6114b38066c4...89569a6ffb3c" }
   diff_url = body["compare"] + ".diff"
   uri = URI.parse(diff_url)
-  response_body = NET::HTTP.get_response(uri).body
+  response_body = Net::HTTP.get_response(uri).body
 
   added_diff = response_body.split("\n").select do |line|
     line[0] == "+" && line[1] != "+" && line[2] != "+"
@@ -22,7 +17,29 @@ post "/payload" do
 
   included_todo = added_diff.select { |diff| diff.include?("TODO: ") }
 
-  included_todo.each do |todo|
-    p todo
+  included_todo.map do |todo|
+    included_todo.gsub(/.*TODO: /, "")
   end
+end
+
+# TODO: あとで変更する
+post "/payload" do
+  body = JSON.parse(params[:payload])
+  diff_url = body["compare"] + ".diff"
+  uri = URI.parse(diff_url)
+  response_body = Net::HTTP.get_response(uri).body
+
+  added_diff = response_body.split("\n").select do |line|
+    line[0] == "+" && line[1] != "+" && line[2] != "+"
+  end
+
+  binding.pry
+
+  included_todo = added_diff.select { |diff| diff.include?("TODO: ") }
+
+  included_todo.map do |todo|
+    included_todo.gsub(/.*TODO: /, "")
+  end
+
+  p included_todo
 end
